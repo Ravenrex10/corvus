b59f522abd5bb69cf7f0007bdaf954e0aebb0771
Ravenrex10 <lucas.colbert@hotmail.com>
2024-07-10 17:31:35 +0200

    ✨input (windows + linux)

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -4,6 +4,7 @@
 #include "game_types.h"
 #include "core/cmemory.h"
 #include "core/event.h"
+#include "core/input.h"
 
 typedef struct application_state{
     game* game_inst;
@@ -18,6 +19,9 @@ typedef struct application_state{
 static b8 initialized = FALSE;
 static application_state app_state;
 
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
+
 b8 application_create(game* game_inst){
     if (initialized) {
         CERROR("application_create called more than once.");
@@ -28,6 +32,8 @@ b8 application_create(game* game_inst){
     
     // Initialize subsystems (logging, events)
     initialize_logging();
+    input_initialize();
+
 
     app_state.is_running = TRUE;
     app_state.is_suspended = FALSE;
@@ -36,6 +42,10 @@ b8 application_create(game* game_inst){
         CFATAL("Failed to initialize event system.");
         return FALSE;
     }
+
+    event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_register(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_register(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
 
     if(!platform_startup(&app_state.platform, game_inst->app_config.name, game_inst->app_config.start_pos_x, game_inst->app_config.start_pos_y, game_inst->app_config.start_width, game_inst->app_config.start_height)){
         return FALSE;
@@ -62,7 +72,7 @@ b8 application_run(){
             app_state.is_running = FALSE;
         }
 
-        if(app_state.is_suspended) {
+        if(!app_state.is_suspended) {
             if (!app_state.game_inst->update(app_state.game_inst, (f32)0)) {
                 CFATAL("Game update failed, shutting down.");
                 app_state.is_running = FALSE;
@@ -75,14 +85,60 @@ b8 application_run(){
                 app_state.is_running = FALSE;
                 break;
             }
+
+            input_update(0);
         }
     }
 
     app_state.is_running = FALSE;
 
+    event_unregister(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_unregister(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
+
     event_shutdown();
+    input_shutdown();
     
     platform_shutdown(&app_state.platform);
 
     return TRUE;
+}
+
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context){
+    switch(code){
+        case EVENT_CODE_APPLICATION_QUIT: {
+            CINFO("EVENT_CODE_APPLICATION_QUIT recieved, shutting down.\n");
+            app_state.is_running = FALSE;
+            return TRUE;
+        }
+    }
+    return TRUE;
+}
+
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context) {
+    if (code == EVENT_CODE_KEY_PRESSED) {
+        u16 key_code = context.data.u16[0];
+        if (key_code == KEY_ESCAPE) {
+            // NOTE: Technically firing an event to itself, but there may be other listeners.
+            event_context data = {};
+            event_fire(EVENT_CODE_APPLICATION_QUIT, 0, data);
+
+            // Block anything else from processing this.
+            return TRUE;
+        } else if (key_code == KEY_A) {
+            // Example on checking for a key
+            CDEBUG("Explicit - A key pressed!");
+        } else {
+            CDEBUG("'%c' key pressed in window.", key_code);
+        }
+    } else if (code == EVENT_CODE_KEY_RELEASED) {
+        u16 key_code = context.data.u16[0];
+        if (key_code == KEY_B) {
+            // Example on checking for a key
+            CDEBUG("Explicit - B key released!");
+        } else {
+            CDEBUG("'%c' key released in window.", key_code);
+        }
+    }
+    return FALSE;
 }
